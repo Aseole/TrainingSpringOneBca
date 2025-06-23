b0839658911935cc8786f7ee118b0ca31770dcc5
Victor Wijaya <42199533+Aseole@users.noreply.github.com>
2025-06-23 14:59:26 +0700

    Add files via upload

---
+++ demo/demo/src/main/java/com/example/demo/BookService.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+@Service
+public class BookService {
+    private List<Book> books = new ArrayList<>();
+    private Long currentId = 1L;
+    public List<Book> getAllBooks() {
+        return books;
+    }
+    public Book addBook(Book book) {
+        book.setId(currentId++);
+        books.add(book);
+        return book;
+    }
+    public Book getBookById(Long id) {
+        return books.stream()
+                .filter(book -> book.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new BookNotFoundException(id));
+    }
+    public void deleteBookById(Long id) {
+        Book book = getBookById(id);
+        books.remove(book);
+    }
+}
